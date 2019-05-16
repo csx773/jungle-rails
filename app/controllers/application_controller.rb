10c7db70714ddf98eb2d424c539bbee926b7e33a
Chris Xiao <csx773@gmail.com>
2019-05-16 20:27:08 +0000

    base user login and session all working

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,18 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  # currentuser login and authentication methods, accessable on ALL view pages
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+  helper_method :current_user
+
+  # mabye can remove since no need to restrict page access right now (branch feature/users)
+  # called on any page(controller) that need authentication to access by:  before_filter :authorize
+  def authorize
+    redirect_to '/login' unless current_user
+  end
+
   private
 
   def cart
