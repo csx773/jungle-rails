dee4de907b102332dff7b4f019ed95d03f7c8937
Chris Xiao <csx773@gmail.com>
2019-05-17 23:40:43 +0000

    step 7 nest resource route done

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,10 @@ Rails.application.routes.draw do
   resources :usertests
   root to: 'products#index'
 
-  resources :products, only: [:index, :show]
+  resources :products, only: [:index, :show] do
+    resources :reviews, only: [:create]
+  end
+
   resources :categories, only: [:show]
 
   resource :cart, only: [:show] do
