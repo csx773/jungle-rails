5d7f991190f7f3eba85bc94806036bacaf0751e4
Chris Xiao <csx773@gmail.com>
2019-05-17 21:12:51 +0000

    admin auth works for all pages

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,5 +1,7 @@
 class Admin::CategoriesController < ApplicationController
     
+    http_basic_authenticate_with name: ENV['username'].to_s, password: ENV['password'].to_s
+
     def index
         @categories = Category.order(id: :desc).all 
     end
