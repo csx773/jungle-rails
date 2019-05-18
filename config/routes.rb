6746a834474b72de84013146fbff17fcfda28f4b
Chris Xiao <csx773@gmail.com>
2019-05-18 03:42:32 +0000

    destroy review path works

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
 
-  resources :usertests
   root to: 'products#index'
 
   resources :products, only: [:index, :show] do
-    resources :reviews, only: [:create]
+    resources :reviews, only: [:create, :destroy]
   end
 
   resources :categories, only: [:show]
